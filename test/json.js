b76adb8f646a17b5ad8ca2b17141a5526c7418fa
John Hobbs <john@velvetcache.org>
2017-11-06 21:43:36 -0600

    Add 'others' section and zip.

--- test/json.js
+++ test/json.js
@@ -2,7 +2,7 @@ var fs = require('fs'),
   assert = require('chai').assert,
   validate = require('jsonschema').validate;
 
-var parrots_json_schema = {
+var json_schema = {
   "$schema": "http://json-schema.org/draft-04/schema#",
   "type": "array",
   "items": {
@@ -25,7 +25,7 @@ var parrots_json_schema = {
 describe("parrots.json", function () {
   it("should validate", function () {
     var parrots_json = JSON.parse(fs.readFileSync(__dirname + '/../parrots.json', 'utf8')),
-      validation_result = validate(parrots_json, parrots_json_schema);
+      validation_result = validate(parrots_json, json_schema);
 
     // Schema errors in mocha are pretty useless, so print some here.
     validation_result.errors.forEach(function (e) {
@@ -43,7 +43,7 @@ describe("parrots.json", function () {
   it("should not have extra gifs", function () {
     var parrots_json = JSON.parse(fs.readFileSync(__dirname + '/../parrots.json', 'utf8'));
     parrots_json.forEach(function(parrot) {
-      assert.ok(fs.existsSync(__dirname + '/../parrots/' + parrot.gif) || fs.existsSync(__dirname + '/../parrots/' + parrot.hd), parrot.name)
+      assert.ok(fs.existsSync(__dirname + '/../parrots/' + parrot.gif) || fs.existsSync(__dirname + '/../parrots/' + parrot.hd), parrot.name);
     });
   });
 
@@ -62,6 +62,50 @@ describe("parrots.json", function () {
     parrot_hd_gifs.forEach(function(gif) {
       if(! fs.statSync(__dirname + '/../parrots/hd/' + gif).isFile()) { return; }
       assert.ok(-1 !== hd_parrots.indexOf('hd/' + gif), gif);
+    });
+  });
+});
+
+describe("other.json", function () {
+  it("should validate", function () {
+    var other_json = JSON.parse(fs.readFileSync(__dirname + '/../other.json', 'utf8')),
+      validation_result = validate(other_json, json_schema);
+
+    // Schema errors in mocha are pretty useless, so print some here.
+    validation_result.errors.forEach(function (e) {
+      var header = "-- " + e.toString() + " ------------",
+        footer = "";
+      for(var i = 0; i < header.length; i++) { footer += '-'; } // lol
+      console.log("\n" + header);
+      console.log(e.instance);
+      console.log(footer + "\n");
+    });
+
+    assert.equal(0, validation_result.errors.length);
+  });
+
+  it("should not have extra gifs", function () {
+    var other_json = JSON.parse(fs.readFileSync(__dirname + '/../other.json', 'utf8'));
+    other_json.forEach(function(other) {
+      assert.ok(fs.existsSync(__dirname + '/../other/' + other.gif) || fs.existsSync(__dirname + '/../other/' + other.hd), other.name);
+    });
+  });
+
+  it("should contain all gifs", function () {
+    var other = JSON.parse(fs.readFileSync(__dirname + '/../other.json', 'utf8')).map(function(other) { return other.gif; }).filter(function(gif) { return gif != undefined; });
+    var other_gifs = fs.readdirSync(__dirname + '/../other');
+    other_gifs.forEach(function(gif) {
+      if(! fs.statSync(__dirname + '/../other/' + gif).isFile()) { return; }
+      assert.ok(-1 !== other.indexOf(gif), gif);
+    });
+  });
+
+  it("should contain all HD gifs", function () {
+    var hd_other = JSON.parse(fs.readFileSync(__dirname + '/../other.json', 'utf8')).map(function(other) { return other.hd; }).filter(function(hd) { return hd != undefined; });
+    var other_hd_gifs = fs.readdirSync(__dirname + '/../other/hd');
+    other_hd_gifs.forEach(function(gif) {
+      if(! fs.statSync(__dirname + '/../other/hd/' + gif).isFile()) { return; }
+      assert.ok(-1 !== hd_other.indexOf('hd/' + gif), gif);
     });
   });
 });
